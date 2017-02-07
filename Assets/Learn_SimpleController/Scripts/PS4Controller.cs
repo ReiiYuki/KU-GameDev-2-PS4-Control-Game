29d81e6fec56e033b16292a9e5842fbb3a289fbe
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-02-07 09:34:31 +0700

    Nothing

---
+++ Assets/Learn_SimpleController/Scripts/PS4Controller.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PS4Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start (){
+	}
+
+	private List<string> pressedButtonList = new List<string>();
+
+	private string []ps4Buttonlist = new string[]{
+		"PS4_Touch",
+		"PS4_O",
+		"PS4_X",
+		"PS4_Triangle",
+		"PS4_Square",
+		"PS4_L1",
+		"PS4_R1",
+		"PS4_Share",
+		"PS4_Options",
+		"PS4_L3",
+		"PS4_R3",
+		"PS4_PS",
+	};
+
+	private string []ps4Axislist = new string[]{
+		"PS4_R_Analog_Hor",
+		"PS4_R_Analog_Ver",
+		"PS4_L_Analog_Hor",
+		"PS4_L_Analog_Ver",
+		"PS4_L2",
+		"PS4_R2",
+		"PS4_DpadX",
+		"PS4_DpadY",
+
+	};
+
+	// Update is called once per frame
+	void Update () {
+		for(int i=0; i<ps4Buttonlist.Length; ++i){
+			if(Input.GetButtonDown(ps4Buttonlist[i])){ 
+				pressedButtonList.Add( ps4Buttonlist[i] );
+			}
+			if(Input.GetButtonUp(ps4Buttonlist[i])){ 
+				pressedButtonList.Remove( ps4Buttonlist[i] );
+			}
+		}
+	}
+
+	void OnGUI(){
+
+		for(int i=0; i<ps4Axislist.Length; ++i){
+			GUILayout.Label(ps4Axislist[i]+" "+Input.GetAxis(ps4Axislist[i]).ToString("N4"));
+		}
+
+		for(int i=0; i<pressedButtonList.Count; ++i){
+			GUILayout.Label(pressedButtonList[i]);
+		}
+	}
+}
---
+++ Assets/Learn_SimpleController/Scripts/TestPS4Controller.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPS4Controller : MonoBehaviour {
+
+	public Transform lCube;
+	public Transform rCube;
+
+	private Vector3 oriPos_LCube = Vector3.zero;
+	private Vector3 oriPos_RCube = Vector3.zero;
+
+	private Vector3 curPos_LCube = Vector3.zero;
+	private Vector3 curPos_RCube = Vector3.zero;
+
+	public float sensitivity = 10f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		oriPos_LCube = lCube.localPosition;
+		oriPos_RCube = rCube.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		curPos_LCube = oriPos_LCube;
+		curPos_RCube = oriPos_RCube;
+
+		curPos_LCube.x += Input.GetAxis("PS4_L_Analog_Hor") * sensitivity;
+		curPos_LCube.y += Input.GetAxis("PS4_L_Analog_Ver") * sensitivity;
+		curPos_LCube.z += Input.GetAxis("PS4_L2") * sensitivity;
+
+		curPos_RCube.x += Input.GetAxis("PS4_R_Analog_Hor") * sensitivity;
+		curPos_RCube.y += Input.GetAxis("PS4_R_Analog_Ver") * sensitivity;
+		curPos_RCube.z += Input.GetAxis("PS4_R2") * sensitivity;
+
+		lCube.localPosition = curPos_LCube;
+		rCube.localPosition = curPos_RCube;
+	}
+}
